029fa6b9439dd6a9d764a6a055ea4fcb876ddad6
Walid Mostefaoui <pro.walid.mostefaoui@gmail.com>
2024-01-26 21:12:51 +0100

    feat: added supabase auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components/ui/Button"
 const Home = () => ( 
   <main className="h-full grid place-items-center"> 
     <div className="text-center">
-    <h1 className="text-3xl">Welcome to Our Boilerplate App</h1>
-    <h3 className="text-xl">This is a brief description of our boilerplate app. It's designed to help you kickstart your next project.</h3>
+      <h1 className="text-3xl">Welcome to Our Boilerplate App</h1>
+      <h3 className="text-xl">This is a brief description of our boilerplate app. It&apos;s designed to help you kickstart your next project.</h3>
 
-    <Button variant="destructive" className="my-3"> This is a button </Button>
+      <Button variant="destructive" className="my-3"> This is a button </Button>
     </div>
   </main> 
 )
